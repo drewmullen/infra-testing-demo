5a46de0663eb051e3e2c9000a126195bcdebcb08
Drew Mullen <drew@nebulaworks.com>
2020-01-17 11:56:13 -0500

    fix merge pipeline relative paths

--- demo-vagrant/failing-profile/controls/example.rb
+++ demo-vagrant/failing-profile/controls/example.rb
@@ -3,14 +3,14 @@
 title "sample section"
 
 # you can also use plain tests
-describe file("/tmp") do
+describe file("/tmp/dir_doesnt_exist") do
   it { should be_directory }
 end
 
 # you add controls here
 control "tmp-1.0" do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
-  title "intended to fail: check to see if /tmp/dir_doesnt_exist exists"             # A human-readable title
+  title "Create /tmp/dir_doesnt_exist directory"             # A human-readable title
   desc "An optional description..."
   describe file("/tmp/dir_doesnt_exist") do                  # The actual test
     it { should be_directory }
