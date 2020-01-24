f0842a03fd801f2676f93a0b581893c5e2b7d4e0
drewmullen <mullen.drew@gmail.com>
2020-01-24 09:14:20 -0500

    increase detail in logged test output

--- images/files/run_inspec.sh
+++ images/files/run_inspec.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/scl enable rh-ruby25 -- /opt/rh/rh-ruby25/root/usr/local/bin/inspec exec /opt/chef/inspec/common --reporter json-min --input-file /opt/chef/inspec/role.yml --chef-license=accept-silent | systemd-cat -t inspec
+/usr/bin/scl enable rh-ruby25 -- /opt/rh/rh-ruby25/root/usr/local/bin/inspec exec /opt/chef/inspec/common --reporter json --input-file /opt/chef/inspec/role.yml --chef-license=accept-silent | systemd-cat -t inspec
